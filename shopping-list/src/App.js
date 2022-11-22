872cd7571a80befd83e0e98a1fafab4ff81bb8a1
Eduardo <el_alienx@hotmail.com>
2022-11-22 10:12:54 +0100

    adding feedback

--- shopping-list/src/App.js
+++ shopping-list/src/App.js
@@ -1,58 +1,56 @@
-import { useEffect, useState } from "react"
-import "./App.css"
-import TodoItem from "./item"
+import { useEffect, useState } from "react";
+import "./App.css";
+import TodoItem from "./item";
 
-import Logo from "./assets/images/logo.svg"
+import Logo from "./assets/images/logo.svg";
 
-import { guidGenerator, readFromStorage, saveToStorage } from "./utils"
-import welcomeImage from "./assets/images/welcomeImage.svg"
+import { guidGenerator, readFromStorage, saveToStorage } from "./utils";
+import welcomeImage from "./assets/images/welcomeImage.svg";
 
 function App() {
+  // Unecesary comments -1
   // all todos tasks
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
+  // Bad variable name -1
+  // is adding sounds like a tax calculation like adding a tip or tax after ordering food online
   // to show the form
-  const [isAdding, setIsAdding] = useState(false)
+  const [isAdding, setIsAdding] = useState(false);
 
   // single input for forms
-  const [name, setName] = useState("")
-  // ....
-  const [price, setPrice] = useState("")
-  const [sortedBy, setSortedBy] = useState("name")
+  const [name, setName] = useState("");
+  // .... // what "...." means
+  const [price, setPrice] = useState("");
+  const [sortedBy, setSortedBy] = useState("name");
 
   // show the accomplished tasks
+  // there is a empty line between the comment and the variable
 
-  const [showCompleted, setShowCompleted] = useState(false)
-
-
-
-
+  const [showCompleted, setShowCompleted] = useState(false);
 
   // To pass the last value to the function
   const toggleShowCompleted = () => {
-    setShowCompleted((prev) => !prev)
-  }
-
+    setShowCompleted((prev) => !prev);
+  };
 
   // to show the form ....toggle
   const toggleShowForm = () => {
-    setIsAdding((prev) => !prev)
-  }
+    setIsAdding((prev) => !prev);
+  };
 
   const changeTodoState = (id, isDone) => {
-    const todoItemIndex = todos.findIndex((item) => item.id === id)
+    const todoItemIndex = todos.findIndex((item) => item.id === id);
     if (todoItemIndex !== -1) {
-      const newTodos = [...todos]
-      newTodos[todoItemIndex].isDone = isDone
-      setTodos(newTodos)
-      saveToStorage(newTodos)
+      const newTodos = [...todos];
+      newTodos[todoItemIndex].isDone = isDone;
+      setTodos(newTodos);
+      saveToStorage(newTodos);
     }
-  }
-
+  };
 
   // to prevent page refresh
   const onSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const newTodos = [
       ...todos,
@@ -62,58 +60,54 @@ function App() {
         price,
         isDone: false,
       },
-    ]
-
-
+    ];
 
     // to update the todos list
-    setTodos(newTodos)
-    saveToStorage(newTodos)
+    setTodos(newTodos);
+    saveToStorage(newTodos);
 
-    setName("")
-    setPrice("")
-    setIsAdding(false)
-  }
-
+    setName("");
+    setPrice("");
+    setIsAdding(false);
+  };
 
   // to call the todoes from local storage
   useEffect(() => {
-    setTodos(readFromStorage())
-  }, [])
-
-
+    setTodos(readFromStorage());
+  }, []);
 
   // [...todos]
   const todosToShow = []
     .concat(todos)
     .sort((a, b) => (a[sortedBy] > b[sortedBy] ? 1 : -1))
     .map((item) => {
-      return !item.isDone && item
+      return !item.isDone && item;
     })
-    .filter(Boolean)
+    .filter(Boolean);
 
   const doneTodos = []
     .concat(todos)
     .sort((a, b) => (a[sortedBy] > b[sortedBy] ? 1 : -1))
     .map((item) => item.isDone && item)
-    .filter(Boolean)
+    .filter(Boolean);
 
   return (
     <div className="todo-app">
-
       <div className="logo-wrapper">
         <img src={Logo} className="logo" alt="logo" />
       </div>
 
       <div className="image">
-
         <img src={welcomeImage} className="image-wrapper" alt=" " />
-
-
       </div>
 
-      <p className="message">welcome to IKEA's shopping list. Here you will be able to create a todo list for the furniture you want to purchase.
-        To get started press the Add new item button and a popup will ask you the name and the price of the item you want to add. You can also add an image after the item is added by touching the camera Icon</p>
+      <p className="message">
+        welcome to IKEA's shopping list. Here you will be able to create a todo
+        list for the furniture you want to purchase. To get started press the
+        Add new item button and a popup will ask you the name and the price of
+        the item you want to add. You can also add an image after the item is
+        added by touching the camera Icon
+      </p>
       <br></br>
 
       <div className="title">Shopping List</div>
@@ -157,8 +151,8 @@ function App() {
               id="name"
               value={name}
               onChange={(e) => {
-                console.log(e)
-                setName(e.target.value)
+                console.log(e);
+                setName(e.target.value);
               }}
               placeholder="Enter Name"
             />
@@ -170,7 +164,7 @@ function App() {
               required
               onKeyPress={(event) => {
                 if (!/[0-9]/.test(event.key)) {
-                  event.preventDefault()
+                  event.preventDefault();
                 }
               }}
               id="price"
@@ -202,7 +196,7 @@ function App() {
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- shopping-list/src/item.js
+++ shopping-list/src/item.js
@@ -1,27 +1,28 @@
-import React from "react"
+import React from "react";
 
+// use a function instead of a constant, just like you did on App.js
 const TodoItem = (props) => {
-    const item = props.item
+  const item = props.item;
 
+  // good destructuring
+  const { id, name, price, isDone } = item;
 
-    const { id, name, price, isDone } = item
-
-    return (
-        <div className="todo-item">
-            <input
-                type="checkbox"
-                id={id}
-                checked={isDone}
-                onChange={(e) => props.changeTodoState(item.id, e.target.checked)}
-            />
-            <label htmlFor={id}>
-                <div className={`${isDone && "done"}`}>
-                    <span>{name}, </span>
-                    <span>{price}</span>
-                </div>
-            </label>
+  return (
+    <div className="todo-item">
+      <input
+        type="checkbox"
+        id={id}
+        checked={isDone}
+        onChange={(e) => props.changeTodoState(item.id, e.target.checked)}
+      />
+      <label htmlFor={id}>
+        <div className={`${isDone && "done"}`}>
+          <span>{name}, </span>
+          <span>{price}</span>
         </div>
-    )
-}
+      </label>
+    </div>
+  );
+};
 
-export default TodoItem
+export default TodoItem;
